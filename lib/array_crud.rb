0dcf6e0485af3317d3c18e5aa15192ecb418d18d
krishnendu basak <krisauz17@yahoo.com>
2022-05-18 16:29:18 +1000

    Arrya CRUD lab is done

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+    week = []  
 end
 
 def create_an_array
+    week = [Monday, Tuesday, Wednesday, Thursday]
+end
+
+def add_element_to_end_of_array(week, Friday)
+    week << "Friday" 
+end
+
+def add_element_to_start_of_array(week, Sunday)
+    week.unshift("Sunday")
+end
+
+def remove_element_from_end_of_array(week)
+    week.pop
+end
+
+def remove_element_from_start_of_array(week)
+    week.shift
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def retrieve_element_from_index(week, 3)
+    week[3]  
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def retrieve_first_element_from_array(week)
+    week.first
+    week[0]  
 end
 
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(week)
+    week.last
+    week[-1]
 end
