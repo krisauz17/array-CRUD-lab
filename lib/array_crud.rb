28364e43529ecdc69de5073556417c616236a894
krishnendu basak <krisauz17@yahoo.com>
2022-05-18 17:28:13 +1000

    Array CRUD lab redone

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,36 +3,43 @@ def create_an_empty_array
 end
 
 def create_an_array
-    week = [Monday, Tuesday, Wednesday, Thursday]
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday"]
 end
 
-def add_element_to_end_of_array(week, Friday)
+def add_element_to_end_of_array
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday"]
     week << "Friday" 
 end
 
-def add_element_to_start_of_array(week, Sunday)
+def add_element_to_start_of_array
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     week.unshift("Sunday")
 end
 
-def remove_element_from_end_of_array(week)
+def remove_element_from_end_of_array
+    week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     week.pop
 end
 
-def remove_element_from_start_of_array(week)
+def remove_element_from_start_of_array
+    week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
     week.shift
-  
 end
 
-def retrieve_element_from_index(week, 3)
+def retrieve_element_from_index
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday"]
     week[3]  
 end
 
-def retrieve_first_element_from_array(week)
+def retrieve_first_element_from_array
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday"]
     week.first
     week[0]  
 end
 
-def retrieve_last_element_from_array(week)
+def retrieve_last_element_from_array
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday"]
     week.last
     week[-1]
 end
+#puts week.inspect
